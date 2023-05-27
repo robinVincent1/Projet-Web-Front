ac40807e36fd9cdbeca613e69648649ef9e4e900
Robin Vincent <robin.vincent@etu.umontpellier.fr>
2023-05-27 14:51:34 +0200

    footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
     return (
         <footer className="relative bottom-0 bg-gradient-to-r from-green-500 via-gray-500 to-black  text-white items-center justify-center text-center p-8">
-        <p className="text-white ">© 2023 MonSiteWeb.com </p>
+        <p className="text-white ">© 2023 projet-web-front-963b.onrender.com </p>
         <p >Me contacter: <em className="underline">robin.vincent@etu.umontpellier.fr</em></p>
       </footer>
     )
